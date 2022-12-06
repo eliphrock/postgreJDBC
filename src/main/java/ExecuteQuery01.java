68c8b0f45f67f993fbd997c4ae7fd5396e8d106e
eliphrock <eliphrock@gmail.com>
2022-12-05 21:26:06 -0500

    execute records

--- src/main/java/ExecuteQuery01.java
+++ src/main/java/ExecuteQuery01.java
@@ -5,21 +5,27 @@ public class ExecuteQuery01 {
         Class.forName("org.postgresql.Driver");
         Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/Techpro", "postgres", "442500");
         Statement st = con.createStatement();
+        //1.Example: Select the country names whose region id's are 1
+        String sql1 ="SELECT country_name FROM countries WHERE region_id = 1";
+        //If you use execute() method, you will get true or false as return. But I want to see the records.
+        boolean result1 = st.execute(sql1);
+        System.out.println("result1 = " + result1);
+        //To see the records we have another method which is "executeQuery()".
 
-       //1.Example: Select the country names whose region id's are 1
-       String sql1="SELECT country_name FROM countries WHERE region_id= 1;";
-       // if you use execute() method you will get true or false as return but I want to see the records
+        ResultSet resultSet1 = st.executeQuery(sql1);
 
-       boolean sqlResult1=st.execute(sql1);
-        System.out.println("sqlResult1="+sqlResult1);
-
-        //to see the records we have another methodwhich is "executeQuery()"
-
-        ResultSet resultSet=st.executeQuery(sql1);
-        while(resultSet.next()){
-            System.out.println(resultSet.getString("country_name"));
-
+        while (resultSet1.next()){
+            System.out.println (resultSet1.getString("country_name"));
         }
 
+        //2.Example: Select the country_id and country_name whose region_id's are greater than 2
+        String sql2 ="SELECT country_id, country_name FROM countries WHERE region_id>2";
+        ResultSet resultSet2 = st.executeQuery(sql2);
+
+        while (resultSet2.next()){
+
+            System.out.println(resultSet2.getString("country_id")+"->"+resultSet2.getString("country_name"));
+
+        }
     }
 }
